212e3e7716d949f42925bcb54bfdd2f9592bedce
yangj <jwyang99@gmail.com>
2016-02-12 09:40:06 -0800

    replace stylus with sass
    used scss style

--- karma.conf.js
+++ karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function (config) {
         loaders: [
           { test: /\.js/, exclude: [/app\/lib/, /node_modules/], loader: 'babel?presets[]=es2015' },
           { test: /\.html/, loader: 'raw' },
-          { test: /\.styl$/, loader: 'style!css!stylus' },
+          { test: /\.scss$/, loader: 'style!css!sass' },
           { test: /\.css$/, loader: 'style!css' }
         ]
       }
